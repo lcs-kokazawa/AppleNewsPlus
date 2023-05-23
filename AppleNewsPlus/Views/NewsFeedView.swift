a0b015047429250855c041935b550dc8bd6a2875
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-05-23 15:16:49 -0400

    We added a scroll view to news feed view. TODO; mAke a helper view to show each news item

--- AppleNewsPlus/Views/NewsFeedView.swift
+++ AppleNewsPlus/Views/NewsFeedView.swift
@@ -9,29 +9,56 @@ import SwiftUI
 
 struct NewsFeedView: View {
     var body: some View {
-        VStack (alignment: .leading) {
-            Image("Farm")
-                .resizable()
-                .scaledToFit()
-            
-            
-            Image("The Globe And Mail")
-                .resizable()
-                .scaledToFit()
-            Text("Martitime farmers holding breath as record-dry spring wings region")
-                .font(.title)
-                .fontWeight(.semibold)
-                .padding(.horizontal)
-            
-            Divider()
-            
-            HStack {
-                Text("1h ago")
-                Spacer()
-                Image(systemName: "ellipsis")
+        ScrollView {
+            VStack (alignment: .leading) {
+                Image("Farm")
+                    .resizable()
+                    .scaledToFit()
+                
+                
+                Image("The Globe And Mail")
+                    .resizable()
+                    .scaledToFit()
+                Text("Martitime farmers holding breath as record-dry spring wings region")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding(.horizontal)
+                
+                Divider()
+                
+                HStack {
+                    Text("1h ago")
+                    Spacer()
+                    Image(systemName: "ellipsis")
+                }
+                .padding([.horizontal, .bottom])
             }
-            .padding([.horizontal, .bottom])
+            
+            VStack (alignment: .leading) {
+                Image("Farm")
+                    .resizable()
+                    .scaledToFit()
+                
+                
+                Image("The Globe And Mail")
+                    .resizable()
+                    .scaledToFit()
+                Text("Martitime farmers holding breath as record-dry spring wings region")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding(.horizontal)
+                
+                Divider()
+                
+                HStack {
+                    Text("1h ago")
+                    Spacer()
+                    Image(systemName: "ellipsis")
+                }
+                .padding([.horizontal, .bottom])
+            }
         }
+       
         .background(Color("DarkGrey"))
         .cornerRadius(25.0)
     }
